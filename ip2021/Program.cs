dd4325ff6a2cb70598ed198673691156bff5585b
Jacek Kawa <jacek.kawa@polsl.pl>
2021-04-08 10:43:17 +0200

    Pierwsza działająca wersja

--- ip2021/Program.cs
+++ ip2021/Program.cs
@@ -6,7 +6,15 @@ namespace ip2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1, b = 2, c = -1;
+            double x1, x2;
+
+            double delta = b * b - 4 * a * c;
+            x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("{0} {1}", x1, x2);
+
         }
     }
 }
