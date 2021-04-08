982e2122cc70e39e76e5a03f9f8f3fd03cde968f
Jacek Kawa <jacek.kawa@polsl.pl>
2021-04-08 10:56:57 +0200

    fix: a <> 1 dawało nieprawidłowy wynik

--- ip2021/Program.cs
+++ ip2021/Program.cs
@@ -23,8 +23,8 @@ namespace ip2021
             if (delta >= 0)
             {
 
-                x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-                x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+                x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b + Math.Sqrt(delta)) / 2 / a;
 
                 Console.WriteLine("dwa pierwiastki: {0} {1}", x1, x2);
             } else
